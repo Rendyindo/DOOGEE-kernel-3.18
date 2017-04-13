c695140379f556923f575fcda3458075156a51f5
Xcelerator1 <darkxcelerator@gmail.com>
2017-04-13 07:46:00 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+mkdir -p out
+export CROSS_COMPILE=~/linaro48/bin/arm-eabi-
+#export USE_CCACHE=1
+export ARCH=arm ARCH_MTK_PLATFORM=mt6580
+#make clean
+#make mrproper
+make -C $PWD O=$PWD/out ARCH=arm v3750_defconfig
+make -j2 -C $PWD O=$PWD/out ARCH=arm
